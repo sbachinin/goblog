0ddf3b0758363948536bfb3079d21e911669d42c
Sergey Bachinin <sbachinin@yandex.ru>
2023-07-09 17:23:39 +0530

    get all titles from articles

--- main.go
+++ main.go
@@ -1,15 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
 var tpl = template.Must(template.ParseFiles("index.html"))
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	tpl.Execute(w, nil)
+
+	entries, err := os.ReadDir("./articles/dev")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	type article struct {
+		Title string
+		Url   string
+	}
+
+	articles := []article{}
+
+	for _, e := range entries {
+		filePath := "./articles/dev/" + e.Name()
+		b, err := os.ReadFile(filePath)
+		if err != nil {
+			fmt.Print(err)
+		}
+
+		title_re := regexp.MustCompile(`^#{2}\s+(.+)`)
+		title := title_re.FindStringSubmatch(string(b))
+
+		if title == nil || len(title) < 2 {
+			continue
+		}
+
+		artcl := article{
+			Title: title[1],
+			Url:   filePath,
+		}
+		articles = append(articles, artcl)
+	}
+
+	tpl.Execute(w, articles)
 }
 
 func main() {
