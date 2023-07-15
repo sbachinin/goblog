b8898cfead437034f2e27c9a9beccada8185c775
sbachinin <ssbachinin@gmail.com>
2023-07-15 17:25:00 +0530

    use md to html converter

--- articleHandler.go
+++ articleHandler.go
@@ -5,7 +5,25 @@ import (
 	"net/http"
 	"os"
 	"text/template"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/html"
+	"github.com/gomarkdown/markdown/parser"
 )
+
+func mdToHTML(md []byte) []byte {
+	// create markdown parser with extensions
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock
+	p := parser.NewWithExtensions(extensions)
+	doc := p.Parse(md)
+
+	// create HTML renderer with extensions
+	htmlFlags := html.CommonFlags | html.HrefTargetBlank
+	opts := html.RendererOptions{Flags: htmlFlags}
+	renderer := html.NewRenderer(opts)
+
+	return markdown.Render(doc, renderer)
+}
 
 var article_tpl = template.Must(template.ParseFiles("article.gohtml"))
 
@@ -16,6 +34,9 @@ func ArticleHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	articleData := ExtractArticleData(string(b))
+
+	md := []byte(articleData.Content)
+	articleData.Content = string(mdToHTML(md))
 
 	article_tpl.Execute(w, articleData)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sbachinin/goblog.git
 
 go 1.20
+
+require github.com/gomarkdown/markdown v0.0.0-20230715013231-a46a3be917c7 // indirect
