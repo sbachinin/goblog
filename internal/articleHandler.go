cbb3ceb0385a08910e69b1d3fc6b39119f5fd037
sbachinin <ssbachinin@gmail.com>
2023-07-16 11:23:17 +0530

    extract markdown converter

--- internal/articleHandler.go
+++ internal/articleHandler.go
@@ -5,25 +5,7 @@ import (
 	"net/http"
 	"os"
 	"text/template"
-
-	"github.com/gomarkdown/markdown"
-	"github.com/gomarkdown/markdown/html"
-	"github.com/gomarkdown/markdown/parser"
 )
-
-func mdToHTML(md []byte) []byte {
-	// create markdown parser with extensions
-	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock
-	p := parser.NewWithExtensions(extensions)
-	doc := p.Parse(md)
-
-	// create HTML renderer with extensions
-	htmlFlags := html.CommonFlags | html.HrefTargetBlank
-	opts := html.RendererOptions{Flags: htmlFlags}
-	renderer := html.NewRenderer(opts)
-
-	return markdown.Render(doc, renderer)
-}
 
 var article_tpl = template.Must(template.ParseFiles("templates/article.gohtml"))
 
@@ -38,5 +20,5 @@ func ArticleHandler(w http.ResponseWriter, r *http.Request) {
 	// md := []byte(articleData.Content)
 	// wrapper := "<div class='article-wrapper'>%v</div>"
 	// fmt.Fprintf(w, wrapper, string(mdToHTML(b)))
-	article_tpl.Execute(w, string(mdToHTML(b)))
+	article_tpl.Execute(w, string(MarkdownToHTML(b)))
 }
